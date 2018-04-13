ef999c8bc8c18e50e1c665940f7f27b40581ad6b
Lei Nelissen <lei@codified.nl>
2018-04-13 14:32:59 +0200

    Add custom .eslintrc

---
+++ packages/react-scripts/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/prop-types": 0,
+        "no-unused-vars": [
+            "error",
+            {
+                "ignoreRestSiblings": true,
+                "varsIgnorePattern": "^(style|styles)$"
+            }
+        ]
+    }
+};
